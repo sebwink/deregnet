19267b15675281f541377cadc3007796505ab59f
sebwink <winkler@informatik.uni-tuebingen.de>
2017-04-11 18:41:33 +0200

    drgnt.py docs

--- bin/drgnt.py
+++ bin/drgnt.py
@@ -11,23 +11,40 @@ from biograph.mapping.gene import GeneIdMapper
 import deregnet
 
 def define_args(parser):
-    parser.add_argument('--graph', dest = 'graph', type = str)
-    parser.add_argument('--scores', dest = 'scores', type = str)
-    parser.add_argument('--gap-cut', default = None, dest = 'gap', type = str)
-    parser.add_argument('--score-column', default = 'score', dest = 'score_col', type = str)
-    parser.add_argument('--id-column', default = 'id', dest = 'id_col', type = str)
-    parser.add_argument('--sep', default = ',', dest = 'sep', type = str)
-    parser.add_argument('--score-id-type', default = None, dest = 'score_id_type', type = str)
-    parser.add_argument('--graph-id-type', default = 'entrez', dest = 'graph_id_type', type = str)
-    parser.add_argument('--graph-id-attr', default = 'name', dest = 'graph_id_attr', type = str)
-    parser.add_argument('--species', default = 'hsa', dest = 'species', type = str)
-    parser.add_argument('--size', default = None, dest = 'size', type = str)
-    parser.add_argument('--suboptimal', default = None, dest = 'suboptimal', type = str)
-    parser.add_argument('--max-overlap-percentage', default = None, dest = 'max_overlap', type = str)
-    parser.add_argument('--root', default = None, dest = 'root', type = str)
-    parser.add_argument('--time-limit', default = None, dest = 'time_limit', type = str)
-    parser.add_argument('--model_sense', default = None, dest = 'model_sense', type = str)
-    parser.add_argument('--output-path', default = os.getcwd(), dest = 'output', type = str)
+    parser.add_argument('--graph', dest = 'graph', type = str, required=True,
+                        help = 'A graphml file containing the graph you want to run drgnt with.')
+    parser.add_argument('--scores', metavar = 'SCORE_FILE', dest = 'scores', type = str, required=True,
+                        help = 'A file containing the scores. See further options below.')
+    parser.add_argument('--score-column', default = 'score', dest = 'score_col', type = str, 
+                        help = 'Column name of (gene) id in your score file. Default: score')
+    parser.add_argument('--id-column', default = 'id', dest = 'id_col', type = str,
+                        help = 'Column name of (gene) id in your score file. Default: id')
+    parser.add_argument('--sep', default = 'comma', dest = 'sep', type = str,
+                        help = 'The column seperator in your score file. Options: comma, tab. Default:comma')
+    parser.add_argument('--score-id-type', default = None, dest = 'score_id_type', type = str,
+                        help = 'Which id type do you have in your score file? Options: entrez, uniprot, ensembl, symbol. Default: same as graph id type')
+    parser.add_argument('--graph-id-type', default = 'entrez', dest = 'graph_id_type', type = str,
+                        help = 'Which id type does the graph have? Options: entrez, uniprot, ensembl, symbol. Default:entrez')
+    parser.add_argument('--graph-id-attr', default = 'name', dest = 'graph_id_attr', type = str,
+                        help = 'Node attribute which contains the relevant id in the graphml. Default: name')
+    parser.add_argument('--species', default = 'hsa', dest = 'species', type = str,
+                        help = 'Species your id\'s refer to. KEGG code: hsa, eco, sce, etc... Default: hsa (Homo Sapiens)')
+    parser.add_argument('--size', default = None, dest = 'size', type = str,
+                        help = 'Size of the resulting subgraph(s). Default : 20')
+    parser.add_argument('--root', default = None, dest = 'root', type = str,
+                        help = 'Specify a root node. Same id type as graph id type!')
+    parser.add_argument('--suboptimal', default = None, dest = 'suboptimal', type = str,
+                        help = 'Number of suboptimal subgraphs you want to find. (Increases runtime)')
+    parser.add_argument('--max-overlap-percentage', default = None, dest = 'max_overlap', type = str,
+                        help = 'How much can suboptimal subgraphs overlap with already found subgraphs. Default: 0')
+    parser.add_argument('--gap-cut', default = None, dest = 'gap', type = str,
+                        help = 'Stop optimization prematurely if current solution within GAP of optimal solution. Default: None')
+    parser.add_argument('--time-limit', default = None, dest = 'time_limit', type = str,
+                        help = 'Set a time limit in seconds. Default: None')
+    parser.add_argument('--model_sense', default = None, dest = 'model_sense', type = str,
+                        help = 'Model sense. \noptions: max, min. Default: max')
+    parser.add_argument('--output-path', default = os.getcwd(), dest = 'output', type = str,
+                        help = 'Folder to which output is written. (Does not have to exist.) Default : cwd')
 
 def parse_sep(sep):
     if sep == 'tab':
